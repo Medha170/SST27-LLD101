43b0e1327df1c26c3d6711756f0712b1a07f43d9
Medha Shree <medha.23bcs10049@sst.scaler.com>
2025-08-25 10:42:32 +0530

    ex05 updated

--- SOLID/ex05/src/Demo05.java
+++ SOLID/ex05/src/Demo05.java
@@ -1,18 +1,16 @@
 public class Demo05 {
-    // Handles 2D resizable shapes (e.g., Rectangle)
     static int areaAfterResize2D(IResizable2D shape) {
         shape.resize(5, 4);
         return ((IAreaService) shape).area();
     }
 
-    // Handles 1D resizable shapes (e.g., Square)
     static int areaAfterResize1D(IResizeable shape) {
-        shape.resize(4); // one side only
+        shape.resize(4); 
         return ((IAreaService) shape).area();
     }
 
     public static void main(String[] args) {
-        System.out.println(areaAfterResize2D(new Rectangle())); // 20
-        System.out.println(areaAfterResize1D(new Square()));    // 16
+        System.out.println(areaAfterResize2D(new Rectangle())); 
+        System.out.println(areaAfterResize1D(new Square()));    
     }
 }
